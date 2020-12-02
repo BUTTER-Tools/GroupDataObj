a229d26ae6a597e6d6d8ebc2e6f23cd995d73ff5
Ryan L. Boyd <ryanb@ryanb.cc>
2020-12-02 12:31:13 +0000

    initial commit

---
+++ GroupDataObj.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace GroupDataObj
+{
+    public class GroupData
+    {
+
+        public List<Person> People { get; set; }
+        public Dictionary<ulong, Tuple<string, int>> TurnTracker {get;set;}
+        
+        public GroupData()
+        {
+            People = new List<Person>();
+        }
+
+    }
+
+    public class Person
+    {
+        public string id { get; set; }
+        public List<string> text { get; set; }
+
+        public Person(string identifier, List<string> text_data)
+        {
+            id = identifier;
+            text = text_data;
+        }
+
+    }
+
+}
